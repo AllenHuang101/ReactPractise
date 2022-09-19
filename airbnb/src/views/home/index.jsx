b7a37e5027df9fbf24474aa48410918a2d7c2a42
Allen <a1262003@gmail.com>
2022-09-19 22:35:55 +0800

    項目搭建-axios 封裝

--- airbnb/src/views/home/index.jsx
+++ airbnb/src/views/home/index.jsx
@@ -1,7 +1,27 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
+import hyRequest from '@/services';
 
 const Home = memo(() => {
-  return <div>Home</div>;
+  // 定義狀態
+  const [highScore, setHighScore] = useState({});
+
+  useEffect(() => {
+    hyRequest.get({ url: '/home/highscore' }).then((res) => {
+      console.log(res);
+      setHighScore(res);
+    });
+  }, []);
+  return (
+    <div>
+      <h2>{highScore.title}</h2>
+      <h4>{highScore.subtitle}</h4>
+      <ul>
+        {highScore.list?.map((item) => {
+          return <li key={item.id}>{item.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
 });
 
 export default Home;
