4772102bb645b29413c261447440faa7b21cbfc0
Allen <a1262003@gmail.com>
2022-09-20 21:12:08 +0800

    Header的封裝

--- airbnb/src/views/home/index.jsx
+++ airbnb/src/views/home/index.jsx
@@ -2,24 +2,9 @@ import React, { memo, useEffect, useState } from 'react';
 import hyRequest from '@/services';
 
 const Home = memo(() => {
-  // 定義狀態
-  const [highScore, setHighScore] = useState({});
-
-  useEffect(() => {
-    hyRequest.get({ url: '/home/highscore' }).then((res) => {
-      console.log(res);
-      setHighScore(res);
-    });
-  }, []);
   return (
     <div>
-      <h2>{highScore.title}</h2>
-      <h4>{highScore.subtitle}</h4>
-      <ul>
-        {highScore.list?.map((item) => {
-          return <li key={item.id}>{item.name}</li>;
-        })}
-      </ul>
+      <h2>Home Page</h2>
     </div>
   );
 });
