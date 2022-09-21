0376f7fc9caeb21d442cca546364cdcda4777065
Allen <a1262003@gmail.com>
2022-09-21 21:03:03 +0800

    panel 切換效果

--- airbnb/src/components/app-header/c-cpns/header-right/index.jsx
+++ airbnb/src/components/app-header/c-cpns/header-right/index.jsx
@@ -1,10 +1,30 @@
 import IconAvatar from '@/assets/svg/icon_avatar';
 import IconGlobal from '@/assets/svg/icon_global';
 import IconMenu from '@/assets/svg/icon_menu';
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { RightWrapper } from './style';
 
 const HeaderRight = memo(() => {
+  // 定義組件內部狀態
+  const [showPanel, setShowPanel] = useState(false);
+
+  // 副作用代碼
+  useEffect(() => {
+    function windowHandlerClick() {
+      setShowPanel(false);
+    }
+    window.addEventListener('click', windowHandlerClick, true);
+
+    return () => {
+      window.removeEventListener('click', windowHandlerClick, true);
+    };
+  }, []);
+
+  // 事件處理函數
+  function profileClickHandler() {
+    setShowPanel(true);
+  }
+
   return (
     <RightWrapper>
       <div className="btns">
@@ -15,9 +35,23 @@ const HeaderRight = memo(() => {
         </span>
       </div>
 
-      <div className="profile">
+      <div className="profile" onClick={profileClickHandler}>
         <IconMenu />
         <IconAvatar />
+
+        {showPanel && (
+          <div className="panel">
+            <div className="top">
+              <div className="item register">註冊</div>
+              <div className="item login">登錄</div>
+            </div>
+            <div className="bottom">
+              <div className="item">出租房源</div>
+              <div className="item">發展體驗</div>
+              <div className="item">幫助</div>
+            </div>
+          </div>
+        )}
       </div>
     </RightWrapper>
   );
